55f4d1bede6dd0f3682e2174e4545c051ea53361
AishShrestha <shresthaayush49@gmail.com>
2024-12-15 14:14:05 +0545

    first commit

---
+++ src/contexts/todoContext.js
@@ -0,0 +1,16 @@
+import { createContext ,useContext} from "react";
+
+
+export const TodoContext = createContext({
+    todos:[],
+    addTodo:(todo)=>{},
+    deleteTodo:(id)=>{},
+    updateTodo:(id,todo)=>{},
+    toggleComplete:(id)=>{}
+});
+
+export const TodoProvider = TodoContext.Provider;
+
+export const useTodo = () =>{
+    return useContext(TodoContext);
+}
